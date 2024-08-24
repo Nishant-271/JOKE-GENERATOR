1a380fa8879c1384b1b3cb1bacb4333492eaee58
Nishant-271 <126049529+Nishant-271@users.noreply.github.com>
2024-08-24 12:44:48 +0530

    Add files via upload
    Generate random jokes using api and javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+const jokeContainer = document.getElementById("joke");
+const search = document.getElementById("search");
+// url of the jooke api ::::
+const url =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () => {
+  jokeContainer.classList.remove("fade");
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      jokeContainer.textContent = `${item.joke}`;
+      jokeContainer.classList.add("fade");
+    });
+};
+search.addEventListener("click", getJoke);
+getJoke();
